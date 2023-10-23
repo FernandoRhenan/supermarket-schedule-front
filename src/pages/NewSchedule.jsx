7d8cbcf3e7583e6446d19998d5fbe3277fc0215e
FernandoRhenan <fernandorhenan9@gmail.com>
2023-10-23 16:18:11 -0300

    GetMonths component

--- src/pages/NewSchedule.jsx
+++ src/pages/NewSchedule.jsx
@@ -1,24 +1,42 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import style from '../../public/styles/pages/newSchedule.module.css'
 import { scheduleFiller, scheduleRange } from '../utils/schedules.js'
 
 const NewSchedule = () => {
 
-	const [dates, setDates] = useState([])
 
-	useEffect(() => {
+	function getDates() {
 		const { minRange, maxRange } = scheduleRange(2)
 		const datesArray = scheduleFiller(minRange, maxRange)
+		return datesArray
+	}
 
-		console.log(datesArray)
-		setDates(datesArray)
-	}, [])
 
+	function GetMonths({ children }) {
+		const monthName = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+		const monthArray = []
+
+		for (let item of getDates()) {
+			monthArray.push(item.month)
+		}
+		const months = Array.from(new Set(monthArray))
+
+		return months.map((month, index) => (
+			<div key={index}>
+				{monthName[month]}
+				{children}
+			</div>
+		));
+
+	}
 
 
 	return (
 		<div className={style.mainContainer}>
 			<div className={style.scheduleContainer}>
+				<GetMonths>
+					<h1>d</h1>
+				</GetMonths>
 				<table>
 					<thead>
 						<tr>
